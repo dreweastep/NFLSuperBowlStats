a45e150243660f8d61dd1de0ca1bbf4a65c10670
dreweastep <dreweastep@gmail.com>
2018-02-21 11:55:40 -0600

    Changed back to empty constructor, and added object instantiation for SuperBowl

--- Project NFL Stats/Project NFL Stats/Program.cs
+++ Project NFL Stats/Project NFL Stats/Program.cs
@@ -3,14 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Project_NFL_Stats
 {
     class Program
     {
+        //DECLARATIONS
+        List<SuperBowl> superBowlList = new List<SuperBowl>();
+
         static void Main(string[] args)
         {
 
-        }
+        }//End of Main method
+
+        private void ReadIn()
+        {
+            const char DELIMITER = ',';
+            const string FILEPATH = @"C:\Users\easandb\OneDrive - dunwoody.edu\Documents\Advanced Programming\Projects\NFLStatistics\Super_Bowl_Projects.csv";
+
+            using(var reader = new StreamReader(FILEPATH))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var elements = line.Split(DELIMITER);
+
+                    SuperBowl thisSuperBowl = new SuperBowl();
+
+                    thisSuperBowl.Date = elements[0];
+                    thisSuperBowl.SuperBowlNumber = elements[1];
+                    thisSuperBowl.Attendance = Int32.Parse(elements[2]);
+                    thisSuperBowl.WinningQB = elements[4];
+                    thisSuperBowl.WinningTeam = elements[5];
+                    thisSuperBowl.WinningTeamPoints = Int32.Parse(elements[6]);
+                    thisSuperBowl.LosingQB = elements[7];
+                    thisSuperBowl.LosingCoach = elements[8];
+                    thisSuperBowl.LosingTeam = elements[9];
+                    thisSuperBowl.LosingTeamPoints = Int32.Parse(elements[10]);
+                    thisSuperBowl.MVP = elements[11];
+                    thisSuperBowl.Stadium = elements[12];
+                    thisSuperBowl.City = elements[13];
+                    thisSuperBowl.State = elements[14];
+
+                    superBowlList.Add(thisSuperBowl);
+
+                }//End of while loop
+            }//End of using block
+
+
+        }//End of ReadIn method
     }
 }
--- Project NFL Stats/Project NFL Stats/SuperBowl.cs
+++ Project NFL Stats/Project NFL Stats/SuperBowl.cs
@@ -3,61 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 
 namespace Project_NFL_Stats
 {
     public class SuperBowl
     {
-        public string Date { get; }
+        public string Date { get; set; }
 
-        public string SuperBowlNumber { get; }
+        public string SuperBowlNumber { get; set; }
 
-        public int Attendance { get; }
+        public int Attendance { get; set; }
 
-        public string WinningQB { get; }
+        public string WinningQB { get; set; }
 
-        public string WinningCoach { get; }
+        public string WinningCoach { get; set; }
 
-        public string WinningTeam { get; }
+        public string WinningTeam { get; set; }
 
-        public int WinningTeamPoints { get; }
+        public int WinningTeamPoints { get; set; }
 
-        public string LosingQB { get; }
+        public string LosingQB { get; set; }
 
-        public string LosingCoach { get; }
+        public string LosingCoach { get; set; }
 
-        public string LosingTeam { get; }
+        public string LosingTeam { get; set; }
 
-        public int LosingTeamPoints { get; }
+        public int LosingTeamPoints { get; set; }
 
-        public string MVP { get; }
+        public string MVP { get; set; }
 
-        public string Stadium { get; }
+        public string Stadium { get; set; }
 
-        public string City { get; }
+        public string City { get; set; }
 
-        public string State { get; }
+        public string State { get; set; }
 
 
-        public SuperBowl(string date, string superBowl, int attendance, string winningQB, string winningCoach, string winningTeam, 
-                         int winningTeamPoints, string losingQB, string losingCoach, string losingTeam, int losingTeamPoints,
-                         string mvp, string stadium, string city, string state)
-        {
-            Date = date;
-            SuperBowlNumber = superBowl;
-            Attendance = attendance;
-            WinningQB = winningQB;
-            WinningTeam = winningTeam;
-            WinningTeamPoints = winningTeamPoints;
-            LosingQB = losingQB;
-            LosingCoach = losingCoach;
-            LosingTeam = losingTeam;
-            LosingTeamPoints = losingTeamPoints;
-            MVP = mvp;
-            Stadium = stadium;
-            City = city;
-            State = state;
-        }
+        //        public SuperBowl(string date, string superBowl, int attendance, string winningQB, string winningCoach, string winningTeam, 
+        //                         int winningTeamPoints, string losingQB, string losingCoach, string losingTeam, int losingTeamPoints,
+        //                         string mvp, string stadium, string city, string state)
+        //        {
+        //            Date = date;
+        //            SuperBowlNumber = superBowl;
+        //            Attendance = attendance;
+        //            WinningQB = winningQB;
+        //            WinningTeam = winningTeam;
+        //            WinningTeamPoints = winningTeamPoints;
+        //            LosingQB = losingQB;
+        //            LosingCoach = losingCoach;
+        //            LosingTeam = losingTeam;
+        //            LosingTeamPoints = losingTeamPoints;
+        //            MVP = mvp;
+        //            Stadium = stadium;
+        //            City = city;
+        //            State = state;
+        //        }
     }
 }
